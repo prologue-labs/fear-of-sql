f72de7a77a7b8159e23c7788c6199fa46a92c225
Sean Moon <sean@prologue-labs.com>
2026-03-06 20:34:16 -0500

    Fix pyright issues

--- src/fear_of_sql/_psycopg_executor.py
+++ src/fear_of_sql/_psycopg_executor.py
@@ -2,6 +2,7 @@ from collections.abc import Mapping, Sequence
 from typing import Any
 
 import psycopg
+from psycopg.rows import dict_row
 
 
 class PsycopgExecutor:
@@ -11,17 +12,17 @@ class PsycopgExecutor:
     async def fetch_one(
         self, sql: str, args: tuple[object, ...]
     ) -> Mapping[str, Any] | None:
-        cursor = self._executor.cursor(row_factory=psycopg.rows.dict_row)
-        await cursor.execute(sql, args or None)
+        cursor = self._executor.cursor(row_factory=dict_row)
+        await cursor.execute(sql, args or None)  # pyright: ignore[reportArgumentType]
         return await cursor.fetchone()
 
     async def fetch_all(
         self, sql: str, args: tuple[object, ...]
     ) -> Sequence[Mapping[str, Any]]:
-        cursor = self._executor.cursor(row_factory=psycopg.rows.dict_row)
-        await cursor.execute(sql, args or None)
+        cursor = self._executor.cursor(row_factory=dict_row)
+        await cursor.execute(sql, args or None)  # pyright: ignore[reportArgumentType]
         return await cursor.fetchall()
 
     async def execute(self, sql: str, args: tuple[object, ...]) -> int:
-        cursor = await self._executor.execute(sql, args or None)
+        cursor = await self._executor.execute(sql, args or None)  # pyright: ignore[reportArgumentType]
         return cursor.rowcount
